d4f4b09fa40dd2e73a357b296d54a779127ad3ab
Andre van Tonder <drevtonder@gmail.com>
2018-05-08 13:41:41 +0800

    Item Tests

---
+++ Solution Files/BL.Test/ItemTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace BL.Tests
+{
+    [TestClass()]
+    public class ItemTests
+    {
+        [TestMethod()]
+        public void ToXElementTest()
+        {
+            //Assign
+            var item = new Item()
+            {
+                Code = "123",
+                Description = "testing",
+                CurrentCount = 123,
+                OnOrder = true
+            };
+
+            var expected = new XElement("item",
+                        new XElement("code", item.Code),
+                        new XElement("description", item.Description),
+                        new XElement("current-count", item.CurrentCount),
+                        new XElement("on-order", item.OnOrder)
+                    );
+
+            //Act
+            var actual = item.ToXElement();
+
+            //Assert
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+
+        [TestMethod()]
+        public void XMLHeaderTest()
+        {
+            //Assign
+            var expected = new XElement("header",
+                            new XElement("name", "Code"),
+                            new XElement("name", "Description"),
+                            new XElement("name", "Current Count"),
+                            new XElement("name", "On Order")
+                        );
+
+            //Act
+            var actual = Item.XMLHeader();
+
+            //Assert
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+    }
+}
