fe16de8bd52b10b07cbba9e968f2bc9009c8fe60
Andre van Tonder <drevtonder@gmail.com>
2018-05-08 13:44:06 +0800

    Stylesheet tests

---
+++ Solution Files/BL.Test/StylesheetTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Tests
+{
+    [TestClass()]
+    public class StylesheetTests
+    {
+        [TestMethod()]
+        public void GetStyleFilenameTest()
+        {
+            //Assign
+            var style = Stylesheet.Style.Table;
+
+            var expected = "table.css";
+
+            //Act
+            var actual = Stylesheet.GetStyleFilename(style);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
